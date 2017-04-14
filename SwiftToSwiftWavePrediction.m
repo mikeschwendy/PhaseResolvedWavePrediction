ca947644c02289a69b0877d583be283938eee951
mikeschwendy <mikeschwendy@users.noreply.github.com>
2017-04-14 16:00:20 -0700

    Working code for SWIFT predictions; predictions are poor.

--- SwiftToSwiftWavePrediction.m
+++ SwiftToSwiftWavePrediction.m
@@ -2,13 +2,21 @@ clc
 clearvars
 close all
 
-swiftDirectory = '/Users/mike/Documents/UW/Research/Data/SanNicolasIslandSwift/29March2017_22_01';
-resultsDirectory = '/Users/mike/Documents/UW/Research/Data/SanNicolasIslandSwift/29March2017_22_01';
+swiftDirectory = '/Users/mike/Documents/UW/Research/Data/LC-DRI data';
+swiftIDs = {'22','23','24','25'};
+swiftDate = '28Mar2017';
+swiftTime = '19_02';
+
 
 swiftFiles = dir([swiftDirectory '/*.mat']);
-numSwifts = length(swiftFiles);
+numSwifts = length(swiftIDs);
 for i = 1:numSwifts
-    load([swiftDirectory '/' swiftFiles(i).name]);
+    dirI = [swiftDirectory '/SWIFT' char(swiftIDs(i)) '_' swiftDate '/SBG/Raw/' datestr(datenum(swiftDate,'ddmmmyyyy'),'yyyymmdd')];
+    fileI = [dirI '/SWIFT' char(swiftIDs(i)) '_SBG_' swiftDate '_' swiftTime '.mat'];
+    if ~exist(fileI,'file')
+        error('SWIFT file not found')
+    end
+    load(fileI)
     imuData(i) = sbgData;
     minLat(i) = min(imuData(i).EkfNav.latitude);
     maxLat(i) = max(imuData(i).EkfNav.latitude);
@@ -34,80 +42,19 @@ end
 meanX_tot = mean(medX);
 meanY_tot = mean(medY);
 
-fig(1) = figure(1);
-subplot(3,1,1)
-hold on
-for i = 1:numSwifts
-    plot(imuData(i).ShipMotion.time_stamp,imuData(i).ShipMotion.heave)
-end
-hold off
-subplot(3,1,2)
-hold on
-% for i = 1:numSwifts
-%     %plot(imuData(i).EkfNav.time_stamp,utm(i).x-meanX_tot)
-%     plot(imuData(i).EkfNav.time_stamp,imuData(i).EkfNav.latitude)
-% end
-%   plot(imuData(1).EkfNav.time_stamp,imuData(1).EkfNav.latitude)
-%    plot(imuData(2).EkfNav.time_stamp,imuData(2).EkfNav.latitude,'--')
-plot(imuData(3).EkfNav.time_stamp,imuData(3).EkfNav.latitude)
-plot(imuData(4).EkfNav.time_stamp,imuData(4).EkfNav.latitude,'--')
-hold off
-%ylim([-500,500])
 
-subplot(3,1,3)
-hold on
-% for i = 1:numSwifts
-%     %plot(imuData(i).EkfNav.time_stamp,utm(i).y-meanY_tot)
-%     plot(imuData(i).EkfNav.time_stamp,imuData(i).EkfNav.longitude)
-% end
-%    plot(imuData(1).EkfNav.time_stamp,imuData(1).EkfNav.longitude)
-%    plot(imuData(2).EkfNav.time_stamp,imuData(2).EkfNav.longitude,'--')
-%plot(imuData(4).EkfNav.time_stamp,imuData(4).EkfNav.longitude,'--')
-hold off
-ylim([0 10])
-%ylim([-500,500])
-
-nLat = 101;
-nLon = 101;
-latVec = linspace(min(minLat),max(maxLat),nLat);
-lonVec = linspace(min(minLon),max(maxLon),nLon);
 t = minTime:dt:maxTime;
 nt = length(t);
-x = nan(numSwifts,nt);
-y = nan(numSwifts,nt);
-z = nan(numSwifts,nt);
+x_swifts = nan(nt,numSwifts);
+y_swifts = nan(nt,numSwifts);
+z_swifts = nan(nt,numSwifts);
 for i = 1:length(imuData)
     [tUnique,indUnique,~] = unique(imuData(i).EkfNav.time_stamp);
-    x(i,:) = interp1(tUnique,utm(i).x(indUnique),t,'linear',NaN);
-    y(i,:) = interp1(tUnique,utm(i).y(indUnique),t,'linear',NaN);
+    x_swifts(:,i) = interp1(tUnique,utm(i).x(indUnique),t,'linear',NaN);
+    y_swifts(:,i) = interp1(tUnique,utm(i).y(indUnique),t,'linear',NaN);
     [tUnique2,indUnique2,~] = unique(imuData(i).ShipMotion.time_stamp);
-    z(i,:) = interp1(tUnique2,imuData(i).ShipMotion.heave(indUnique2),t,'linear',NaN);
+    z_swifts(:,i) = interp1(tUnique2,imuData(i).ShipMotion.heave(indUnique2),t,'linear',NaN);
 end
-
-fig(2) = figure(2);
-subplot(2,1,1)
-hold on
-plot(t,x(1,:)-x(2,:))
-plot(t,x(3,:)-x(4,:))
-plot(t,x(1,:)-x(4,:))
-hold off
-%ylim([-40 40])
-subplot(2,1,2)
-hold on
-plot(t,y(1,:)-y(2,:))
-plot(t,y(3,:)-y(4,:))
-plot(t,y(1,:)-y(4,:))
-hold off
-%ylim([-40 40])
-
-% fig(2) = figure(2);
-% for i = 1:nt
-%     scatter(x(:,i)-medX,y(:,i),40,z(:,i))
-%     xlim([3.2e5,3.23e5])
-%     %ylim([3.665e6,3.675e6])
-%     axis equal
-%     pause(.01);
-% end
 
 for i = 1:numSwifts
     goodInd = imuData(i).ShipMotion.time_stamp>0.5e8;
@@ -118,26 +65,75 @@ for i = 1:numSwifts
 end
 %%
 fig(3) = figure(3); clf(fig(3))
-%subplot(3,1,1)
 hold on
 for i = 1:numSwifts
-       plot(f_z(i,:),S_z(i,:),'-k') 
+    plot(f_z(i,:),S_z(i,:),'-k')
 end
-%hold off
-%set(gca,'XScale','log','YScale','log')
-%set(gca,'XScale','linear','YScale','linear')
-%subplot(3,1,2)
-%hold on
 for i = 1:numSwifts
-       plot(f_u(i,:),S_u(i,:)./((2*pi*f_u).^2),'-b') 
+    plot(f_u(i,:),S_u(i,:)./((2*pi*f_u).^2),'-b')
 end
-%hold off
-%set(gca,'XScale','log','YScale','log')
-%subplot(3,1,3)
-%hold on
 for i = 1:numSwifts
-       plot(f_v(i,:),S_v(i,:)./((2*pi*f_v).^2),'-r') 
+    plot(f_v(i,:),S_v(i,:)./((2*pi*f_v).^2),'-r')
 end
 hold off
 set(gca,'XScale','log','YScale','log')
-%set(gca,'XScale','linear','YScale','linear')
+
+%%
+fig(4) = figure(4); clf(fig(4));
+hold on
+for i = 1:numSwifts
+    scatter((utm(i).x-meanX_tot)/1e3,(utm(i).y-meanY_tot)/1e3,6,imuData(i).EkfNav.time_stamp)
+end
+hold off
+xlim([-0.2 0.2])
+ylim([-0.2 0.2])
+xlabel('x [km]')
+ylabel('y [km]')
+
+%%
+% set time bursts duration/delay etc
+T_meas = 30; %sec
+T_pred = 30; %sec
+T_delay = 0; %sec
+overlap = 1;
+
+% set least squares model values
+k = logspace(-3,-1,51)*2*pi;
+theta_wavenumber = linspace(-180,180,10)*pi/180;
+theta_wavenumber = theta_wavenumber(2:end);
+reg_factor = 1e0;
+    
+for i = 1:numSwifts
+    [z_pred(:,:,i),z_truth(:,:,i),t_pred(:,:,i)] = runLeastSquaresPrediction_Swifts(...
+        x_swifts,y_swifts,z_swifts,i,0.2,...
+        k,theta_wavenumber,reg_factor,T_meas,T_pred,T_delay,overlap);
+end
+
+fig(6) = figure(6); clf(fig(6));
+for i = 1:numSwifts
+    subplot(4,1,i)
+    hold on
+    plot(t_pred(:,:,i)',z_pred(:,:,i)','-k')
+    plot((0:length(t)-1)*0.2,z_swifts(:,i),'-')
+    hold off
+    ylim([-2 2])
+end
+
+fig(7) = figure(7); clf(fig(7));
+H_sig = 2;
+for i = 1:numSwifts
+    z_truth_i = z_truth(:,:,i);
+    z_pred_i = z_pred(:,:,i);
+    subplot(2,2,i)
+    hold on
+    plot(z_truth_i(:),z_pred_i(:),'.')
+    [z_regress,z_regress_int,z_regress_resid,~,regress_stats] = regress(z_truth_i(:),z_pred_i(:));
+    regress_r2 = regress_stats(1);
+    plot([-H_sig H_sig],[-H_sig H_sig],'--k')
+    plot([-H_sig H_sig],[-H_sig H_sig].*z_regress,'--b')
+    hold off
+    xlim([-H_sig H_sig])
+    ylim([-H_sig H_sig])
+
+end
+
