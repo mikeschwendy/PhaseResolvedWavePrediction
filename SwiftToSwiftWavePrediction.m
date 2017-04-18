f2eac41beff5f25de8202fa8f946079fa0a9b34c
mikeschwendy <mikeschwendy@users.noreply.github.com>
2017-04-18 08:52:13 -0700

    Decent surface reconstruction from SWIFTs
    runLeastSquaresPrediction_Swifts modified to allow for multiple target
    points.  Important realization - T_delay=0 means prediction happens
    right after measurements, not during measurements.  T_delay must be
    negative in these measurements.

--- SwiftToSwiftWavePrediction.m
+++ SwiftToSwiftWavePrediction.m
@@ -1,17 +1,18 @@
-clc
+%clc
 clearvars
 close all
 
 swiftDirectory = '/Users/mike/Documents/UW/Research/Data/LC-DRI data';
 swiftIDs = {'22','23','24','25'};
-swiftDate = '28Mar2017';
-swiftTime = '19_02';
+%swiftDate = '28Mar2017'; swiftTime = '19_02';
+swiftDate = '02Apr2017'; swiftDate0 = '02Apr2017';  swiftTime = '23_02';
+
 
 
 swiftFiles = dir([swiftDirectory '/*.mat']);
 numSwifts = length(swiftIDs);
 for i = 1:numSwifts
-    dirI = [swiftDirectory '/SWIFT' char(swiftIDs(i)) '_' swiftDate '/SBG/Raw/' datestr(datenum(swiftDate,'ddmmmyyyy'),'yyyymmdd')];
+    dirI = [swiftDirectory '/SWIFT' char(swiftIDs(i)) '_' swiftDate0 '/SBG/Raw/' datestr(datenum(swiftDate,'ddmmmyyyy'),'yyyymmdd')];
     fileI = [dirI '/SWIFT' char(swiftIDs(i)) '_SBG_' swiftDate '_' swiftTime '.mat'];
     if ~exist(fileI,'file')
         error('SWIFT file not found')
@@ -41,9 +42,12 @@ for i = 1:numSwifts
 end
 meanX_tot = mean(medX);
 meanY_tot = mean(medY);
+for i = 1:numSwifts
+    utm(i).x = utm(i).x - meanX_tot;
+    utm(i).y = utm(i).y - meanY_tot;
+end
 
-
-t = minTime:dt:maxTime;
+t = min(minTime):dt:max(maxTime);
 nt = length(t);
 x_swifts = nan(nt,numSwifts);
 y_swifts = nan(nt,numSwifts);
@@ -69,71 +73,116 @@ hold on
 for i = 1:numSwifts
     plot(f_z(i,:),S_z(i,:),'-k')
 end
-for i = 1:numSwifts
-    plot(f_u(i,:),S_u(i,:)./((2*pi*f_u).^2),'-b')
-end
-for i = 1:numSwifts
-    plot(f_v(i,:),S_v(i,:)./((2*pi*f_v).^2),'-r')
-end
+% for i = 1:numSwifts
+%     plot(f_u(i,:),S_u(i,:)./((2*pi*f_u).^2),'-b')
+% end
+% for i = 1:numSwifts
+%     plot(f_v(i,:),S_v(i,:)./((2*pi*f_v).^2),'-r')
+% end
 hold off
-set(gca,'XScale','log','YScale','log')
+set(gca,'XScale','linear','YScale','linear','xlim',[0 0.5],'ylim',[0 10])
 
 %%
 fig(4) = figure(4); clf(fig(4));
 hold on
 for i = 1:numSwifts
-    scatter((utm(i).x-meanX_tot)/1e3,(utm(i).y-meanY_tot)/1e3,6,imuData(i).EkfNav.time_stamp)
+    scatter(utm(i).x,utm(i).y,6,imuData(i).EkfNav.time_stamp)
 end
 hold off
-xlim([-0.2 0.2])
-ylim([-0.2 0.2])
+xlim([-100 100])
+ylim([-100 100])
 xlabel('x [km]')
 ylabel('y [km]')
 
 %%
+fig(5) = figure(5); clf(fig(5));
+for i = 1:numSwifts
+    subplot(4,1,i)
+    plot(t/1e6,z_swifts(:,i),'k')
+    xlim([60 180])
+    ylim([-2 2])
+end
+%%
 % set time bursts duration/delay etc
 T_meas = 30; %sec
 T_pred = 30; %sec
-T_delay = 10; %sec
+T_delay = -30; %sec
 overlap = 1;
 
 % set least squares model values
-k = logspace(-3,-1,51)*2*pi;
-theta_wavenumber = linspace(-180,180,10)*pi/180;
+k = logspace(-3,-1,41)*2*pi;
+theta_wavenumber = linspace(-180,180,19)*pi/180;
 theta_wavenumber = theta_wavenumber(2:end);
-reg_factor = 1e0;
+reg_factor = 1e3;
+
+% x_array = x_swift(:,setdiff(1:numSwift,ind_target));
+% y_array = y_swift(:,setdiff(1:numSwift,ind_target));
+% z_array = z_swift(:,setdiff(1:numSwift,ind_target));
+% x_target = x_swift(:,ind_target);
+% y_target = y_swift(:,ind_target);
+% z_target = nan(size(x_target));
+
+[x_target,y_target] = meshgrid(linspace(-100,100,20),linspace(-100,100,21));
+[ny_target,nx_target] = size(x_target);
+[z_target_pred,t_pred] = runLeastSquaresPrediction_Swifts(...
+    x_target,y_target,x_swifts,y_swifts,z_swifts,0.2,...
+    k,theta_wavenumber,reg_factor,T_meas,T_pred,T_delay,overlap);
+[num_bursts,Nt_pred,~] = size(z_target_pred);
+z_target_pred = reshape(z_target_pred,[num_bursts,Nt_pred,ny_target,nx_target]);
+
+%%
+if true
+fig(8) = figure(8); clf(fig(8));
+for j = 2:size(z_target_pred,1)
+    for i = 1:size(z_target_pred,2)
+        clf(fig(8));
+        subplot(4,1,1:3)
+        pcolor(x_target,y_target,squeeze(z_target_pred(j,i,:,:)))
+        shading('flat')
+        t_i = t_pred(j,i,1);
+        t_ind = round(t_i/0.2);
+        hold on
+        H = scatter(x_swifts(t_ind,:),y_swifts(t_ind,:),100,z_swifts(t_ind,:),'filled');
+        H.MarkerEdgeColor = 'k';
+        hold off
+        set(gca,'CLim',[-1 1],'XLim',[-100 100],'YLim',[-100 100])
+        subplot(4,1,4)
+        plot((0:nt-1)'*0.2*ones(1,numSwifts),z_swifts(:,:))
+        hold on
+        plot(t_i*ones(numSwifts,1),z_swifts(t_ind,:),'o')
+        hold off
+        set(gca,'YLim',[-1 1],'XLim',[100 200])
+        pause(.1)
+    end
+end
+end
+
+if false
+    fig(6) = figure(6); clf(fig(6));
+    for i = 1:numSwifts
+        subplot(4,1,i)
+        hold on
+        plot(t_pred(:,:,i)',z_pred(:,:,i)','-k')
+        plot((0:length(t)-1)*0.2,z_swifts(:,i),'-')
+        hold off
+        ylim([-2 2])
+    end
     
-for i = 1:numSwifts
-    [z_pred(:,:,i),z_truth(:,:,i),t_pred(:,:,i)] = runLeastSquaresPrediction_Swifts(...
-        x_swifts,y_swifts,z_swifts,i,0.2,...
-        k,theta_wavenumber,reg_factor,T_meas,T_pred,T_delay,overlap);
-end
-
-fig(6) = figure(6); clf(fig(6));
-for i = 1:numSwifts
-    subplot(4,1,i)
-    hold on
-    plot(t_pred(:,:,i)',z_pred(:,:,i)','-k')
-    plot((0:length(t)-1)*0.2,z_swifts(:,i),'-')
-    hold off
-    ylim([-2 2])
-end
-
-fig(7) = figure(7); clf(fig(7));
-H_sig = 2;
-for i = 1:numSwifts
-    z_truth_i = z_truth(:,:,i);
-    z_pred_i = z_pred(:,:,i);
-    subplot(2,2,i)
-    hold on
-    plot(z_truth_i(:),z_pred_i(:),'.')
-    [z_regress,z_regress_int,z_regress_resid,~,regress_stats] = regress(z_truth_i(:),z_pred_i(:));
-    regress_r2 = regress_stats(1);
-    plot([-H_sig H_sig],[-H_sig H_sig],'--k')
-    plot([-H_sig H_sig],[-H_sig H_sig].*z_regress,'--b')
-    hold off
-    xlim([-H_sig H_sig])
-    ylim([-H_sig H_sig])
-
+    fig(7) = figure(7); clf(fig(7));
+    H_sig = 2;
+    for i = 1:numSwifts
+        z_truth_i = z_truth(:,:,i);
+        z_pred_i = z_pred(:,:,i);
+        subplot(2,2,i)
+        hold on
+        plot(z_truth_i(:),z_pred_i(:),'.')
+        [z_regress,z_regress_int,z_regress_resid,~,regress_stats] = regress(z_truth_i(:),z_pred_i(:));
+        regress_r2 = regress_stats(1);
+        plot([-H_sig H_sig],[-H_sig H_sig],'--k')
+        plot([-H_sig H_sig],[-H_sig H_sig].*z_regress,'--b')
+        hold off
+        xlim([-H_sig H_sig])
+        ylim([-H_sig H_sig])
+    end
 end
 
--- runLeastSquaresPrediction_Swifts.m
+++ runLeastSquaresPrediction_Swifts.m
@@ -1,43 +1,38 @@
-function [z_target_pred,z_target_truth,t_pred] = runLeastSquaresPrediction_Swifts(...
-    x_swift,y_swift,z_swift,ind_target,dt,...
+function [z_target_pred,t_pred] = runLeastSquaresPrediction_Swifts(...
+    x_target,y_target,x_array,y_array,z_array,dt,...
     k,theta,reg_factor,T_meas,T_pred,T_delay,overlap)
 
 
 % reshape/initialize arrays
-x_target = x_swift(:,ind_target);
-y_target = y_swift(:,ind_target);
-z_target = z_swift(:,ind_target);
-[Nt,numSwift] = size(x_swift);
+x_target = x_target(:);
+y_target = y_target(:);
+num_points_target = length(x_target);
+[Nt_tot,num_points_array] = size(x_array);
 Nt_meas = round(T_meas/dt);
 Nt_pred = round(T_pred/dt);
 Nt_delay = round(T_delay/dt);
-t_meas_start_ind = round(1:(Nt_meas*overlap):(Nt-Nt_meas+1));
+t_meas_start_ind = round(1:(Nt_meas*overlap):(Nt_tot-Nt_meas+1));
 t_pred_start_ind = t_meas_start_ind + Nt_meas + Nt_delay;
 num_bursts = length(t_meas_start_ind);
-t_pred = nan(num_bursts,Nt_pred);
-z_target_pred = nan(num_bursts,Nt_pred);
-z_target_truth = nan(num_bursts,Nt_pred);
-x_array = x_swift(:,setdiff(1:numSwift,ind_target));
-y_array = y_swift(:,setdiff(1:numSwift,ind_target));
-z_array = z_swift(:,setdiff(1:numSwift,ind_target));
+t_pred = nan(num_bursts,Nt_pred,num_points_target);
+z_target_pred = nan(num_bursts,Nt_pred,num_points_target);
+x_target_vect = repmat(x_target(:)',[Nt_pred,1]);
+y_target_vect = repmat(y_target(:)',[Nt_pred,1]);
 
 % loop over bursts, make least squares prediction
-for i = 1:num_bursts
+for i = 1:5%num_bursts
     % collect buoy array measurements for this burst
     t_target_ind = t_meas_start_ind(i):(t_meas_start_ind(i)+Nt_meas-1);
-    x_array_vect = reshape(x_array(t_target_ind,:),[1,Nt_meas*(numSwift-1)]);
-    y_array_vect = reshape(y_array(t_target_ind,:),[1,Nt_meas*(numSwift-1)]);
-    z_array_vect = reshape(z_array(t_target_ind,:),[1,Nt_meas*(numSwift-1)]);
-    t_array_vect = reshape(repmat((t_target_ind'-1)*dt,[(numSwift-1),1]),[1,Nt_meas*(numSwift-1)]);
+    x_array_vect = reshape(x_array(t_target_ind,:),[1,Nt_meas*num_points_array]);
+    y_array_vect = reshape(y_array(t_target_ind,:),[1,Nt_meas*num_points_array]);
+    z_array_vect = reshape(z_array(t_target_ind,:),[1,Nt_meas*num_points_array]);
+    t_array_vect = reshape(repmat((t_target_ind'-1)*dt,[num_points_array,1]),[1,Nt_meas*num_points_array]);
     % set time vector for prediction
     t_pred_ind = t_pred_start_ind(i):(t_pred_start_ind(i)+Nt_pred-1);
-    t_pred(i,:) = (t_pred_ind'-1)*dt;
-    if max(t_pred_ind)<Nt
-        % least squares calculation
-        [z_target_pred_vect,~] = leastSquaresWavePropagation_2D(z_array_vect,...
-            t_array_vect,x_array_vect,y_array_vect,...
-            t_pred(i,:),x_target(t_pred_ind,:),y_target(t_pred_ind,:),k,theta,reg_factor);
-        z_target_pred(i,:) = reshape(z_target_pred_vect,[1,Nt_pred]);
-        z_target_truth(i,:) = z_target(t_pred_ind);
-    end
+    t_pred(i,:,:) = repmat((t_pred_ind'-1)*dt,[1,1,num_points_target]);
+    % least squares calculation
+    [z_target_pred_vect,~] = leastSquaresWavePropagation_2D(z_array_vect,...
+        t_array_vect,x_array_vect,y_array_vect,...
+        t_pred(i,:,:),x_target_vect(:),y_target_vect(:),k,theta,reg_factor);
+    z_target_pred(i,:,:) = reshape(z_target_pred_vect,[1,Nt_pred,num_points_target]);
 end
